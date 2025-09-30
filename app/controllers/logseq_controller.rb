6adac59561b6a09bd12fe2c581b470658fc58062
hyunho <oksknoh@gmail.com>
2025-10-01 03:05:36 +0900

    render logseq journal in home

--- app/controllers/logseq_controller.rb
+++ app/controllers/logseq_controller.rb
@@ -11,11 +11,18 @@ class LogseqController < ApplicationController
   def show
     dir_path = "/Users/hyunho/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/Base/journals/"
     md_file = File.read("#{dir_path}/#{params[:id]}.md")
+
+    # 단독 하이픈 라인 제거
+    md_file = md_file.gsub(/^\s*-\s*$/, '')
+
     renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
-    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    markdown = Redcarpet::Markdown.new(renderer, extensions = { autolink: true })
+    puts "md_file"
+    puts md_file
 
     
     @content = markdown.render(md_file)
+    puts @content
     
     render "logseq/show", locals: { content: @content }
   end
---
+++ app/helpers/logseq_helper.rb
@@ -0,0 +1,2 @@
+module LogseqHelper
+end
